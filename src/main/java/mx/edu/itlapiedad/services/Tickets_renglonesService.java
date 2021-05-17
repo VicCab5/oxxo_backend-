e516d0ab74587d4dfdcfdbe2a1c12acaefc3e7c4
naty <yayi.17.esp@gmail.com>
2021-05-17 00:53:00 -0500

    Agregue tickets_renglones

---
+++ src/main/java/mx/edu/itlapiedad/services/Tickets_renglonesService.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package mx.edu.itlapiedad.services;
+
+import java.util.List;
+
+
+import mx.edu.itlapiedad.models.Tickets_renglones;
+
+/**
+ * @author natal
+ *
+ */
+public interface Tickets_renglonesService {
+	List<Tickets_renglones> consultarTickets_renglones(); 
+	
+	Tickets_renglones buscarTickets_renglones(int id);
+	
+	Tickets_renglones insertarTickets_renglones(Tickets_renglones TR);
+	
+	void actualizarTickets_renglones(Tickets_renglones TR);
+	
+}
